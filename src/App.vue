13cf5de1a32a48c28f416a5515291d204badbfc2
lindabettini <linda.bettini@hotmail.it>
2022-02-10 23:00:47 +0100

    complete header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-	<p class="text-muted text-center">ciao</p>
+	<div>
+		<Header />
+		<Main />
+	</div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 export default {
 	name: "App",
-	components: {},
+	components: {
+		Header,
+		Main,
+	},
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+	<header><img src="../assets/img/logo.png" alt="" /></header>
+</template>
+
+<script>
+export default {
+	name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+	height: 70px;
+	background-color: grey;
+	padding: 0 40px;
+	display: flex;
+	align-items: center;
+	img {
+		height: 50px;
+	}
+}
+</style>

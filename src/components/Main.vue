c569dbcd599ff9ed7514693e05613064ba76361d
lindabettini <linda.bettini@hotmail.it>
2022-02-10 23:37:38 +0100

    import api

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,26 @@
 <template>
-	<main></main>
+	<main>
+		<div class="container">
+			<section id="albums"></section>
+		</div>
+	</main>
 </template>
 
 <script>
+import axios from "axios";
 export default {
 	name: "Main",
+	data() {
+		return {
+			albums: [],
+		};
+	},
+	created() {
+		axios.get("https://flynn.boolean.careers/exercises/api/array/music/").then((res) => {
+			this.albums = res.data.response;
+		});
+	},
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>

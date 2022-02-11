126f352d783e63e0dac36a654adfad67c84c1e03
lindabettini <linda.bettini@hotmail.it>
2022-02-11 16:05:43 +0100

    add select e flex

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-	<header><img src="../assets/img/logo.png" alt="" /></header>
+	<header>
+		<div class="row align-items-center h-100">
+			<div class="col-6 d-flex align-items-center">
+				<img src="../assets/img/logo.png" alt="" />
+			</div>
+			<div class="col-6 d-flex align-items-center justify-content-end">
+				<input class="align-self-end" type="select" />
+			</div>
+		</div>
+	</header>
 </template>
 
 <script>
@@ -13,10 +22,8 @@ header {
 	height: 70px;
 	background-color: rgb(46, 58, 70);
 	padding: 0 40px;
-	display: flex;
-	align-items: center;
-	img {
-		height: 50px;
-	}
+}
+img {
+	height: 50px;
 }
 </style>
